bef70b5790b803c5050ae15c203b5ed2cc995661
bo cheng <cgs05521@gmail.com>
2019-12-21 16:01:47 +0800

    猜對次數

--- ran.py
+++ ran.py
@@ -1,11 +1,13 @@
 import random
-
+num = 1
+num = int(num)
 r = random.randint(1,100)
 while True:
 	ge = input('請猜數字: ')
 	ge = int(ge)
+	num = num + 1
 	if r == ge:
-		print('恭喜猜對')
+		print('恭喜猜對,總共猜',num,'次')
 		break
 	elif r > ge:
 		print('比答案小')
