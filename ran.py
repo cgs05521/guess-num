aec2e35e9bbd667d179468cf4cc5b4646b8d8e34
bo cheng <cgs05521@gmail.com>
2019-12-21 16:13:23 +0800

    新增次數

--- ran.py
+++ ran.py
@@ -1,11 +1,11 @@
 import random
-num = 1
+num = 0
 num = int(num)
 r = random.randint(1,100)
 while True:
 	ge = input('請猜數字: ')
 	ge = int(ge)
-	num = num + 1
+	num += 1
 	if r == ge:
 		print('恭喜猜對,總共猜',num,'次')
 		break
@@ -15,3 +15,4 @@ while True:
 		print('比答案大')
 	else:
 		print('猜錯')
+	print('這是你猜的第',num,'次')
